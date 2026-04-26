22c773fde7d6d6f8b1ab7c3e67b353081d37438d
mod-elise <mod@kijko.com>
2026-04-26 03:18:34 +0100

    checking word

--- wordle.py
+++ wordle.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 
+def checkWord(word):
+    print(characters[1].get())
 
+#-----------------------------------------
 def validate(P):
     if len(P) == 0:
         return True
@@ -10,7 +13,8 @@ def validate(P):
         # Anything else, reject it
         return False
 
-
+word = "SCARE"
+characters = []
 root = tk.Tk()
 root.title("Wordle")
 
@@ -19,10 +23,19 @@ vcmd = (root.register(validate), "%P")
 r1c1 = tk.Entry(root, width=1, validate="key", validatecommand=vcmd, font=("Arial", 18))
 
 for i in range(25):
-    character = tk.Entry(
-        root, width=1, validate="key", validatecommand=vcmd, font=("Arial", 18)
-    )
-    character.grid(row=i // 5, column=i % 5)
+    characters.append( tk.Entry(
+        root, width=1, validate="key",  validatecommand=vcmd, font=("Arial", 18)
+    ))
+    characters[i].grid(row=i // 5, column=i % 5)
 
+tk.Button(
+    root,
+    text="CHECK",
+    width=10,
+    height=4,
+    bg="LIGHT GREEN",
+    command=lambda: checkWord(word)
+    ,
+).grid(row=6, columnspan =5)
 
 root.mainloop()
