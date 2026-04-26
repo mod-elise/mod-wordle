1e7d0f42b7ca360338b36251e358b577405b3a3f
mod-elise <mod@kijko.com>
2026-04-26 01:29:27 +0100

    format correction

--- wordle.py
+++ wordle.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 
 
-
 def validate(P):
     if len(P) == 0:
         return True
@@ -10,22 +9,20 @@ def validate(P):
     else:
         # Anything else, reject it
         return False
-    
+
 
 root = tk.Tk()
 root.title("Wordle")
 
-vcmd = (root.register(validate), '%P')
+vcmd = (root.register(validate), "%P")
 
 r1c1 = tk.Entry(root, width=1, validate="key", validatecommand=vcmd, font=("Arial", 18))
 
 for i in range(25):
-    character = tk.Entry(root, width=1, validate="key", validatecommand=vcmd, font=("Arial", 18))
-    character.grid(row=i // 5, column=i%5)
-
-
-
-
+    character = tk.Entry(
+        root, width=1, validate="key", validatecommand=vcmd, font=("Arial", 18)
+    )
+    character.grid(row=i // 5, column=i % 5)
 
 
 root.mainloop()
